473bb74dfc6b2dba1148b121411a80d03086354d
Marcello_ubuntu <marcellonovaes98@gmail.com>
2024-05-25 09:47:38 -0300

    add os arquivos necessários para a publicação no vercel

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $("#tel").mask("(00) 0 0000-0000", {
+        placeholder: "(21) 9 8765-4321"
+    })
+    $("#cadastro").validate({
+        rules: {
+            cName: {
+                required: true,
+                minlength:3,
+            },
+            email: {
+                required: true,
+                email:true,
+            },
+            tel: {
+                required: false,
+                minlength: 16,
+            },
+            message: {
+                required: true,
+                minlength: 10,
+            },
+        },
+        messages: {
+            cName: "Por favor insira seu nome completo!",
+            message: "Por favor insira algum assunto!",
+        },
+        invalidHandler: function(e, validator) {
+            let camposIncorretos = validator.numberOfInvalids();
+            if (camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        },
+    })
+})
